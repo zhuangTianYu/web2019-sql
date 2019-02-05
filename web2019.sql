a88990e4fbb8a6010bc440a37e7d98b0a04d689d
Zhuang TianYu <zhuangtianyu@weimingedu.com>
2019-02-06 02:40:28 +0800

    创建 comment 表脚本

--- web2019.sql
+++ web2019.sql
@@ -8,4 +8,15 @@ CREATE TABLE IF NOT EXISTS `article` (
    `createTime` DATE NOT NULL COMMENT '创建时间',
    `updateTime` DATE COMMENT '更新时间',
    PRIMARY KEY ( `id` )
-) COMMENT = '文章表' ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) COMMENT = '文章表' ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `comment` (
+   `id` INT UNSIGNED AUTO_INCREMENT COMMENT '主键',
+   `articleID` INT NOT NULL COMMENT '文章id',
+   `userName` VARCHAR(30) NOT NULL COMMENT '用户名',
+   `userID` INT COMMENT '用户id',
+   `content` TEXT NOT NULL COMMENT '内容',
+   `createTime` DATE NOT NULL COMMENT '创建时间',
+   `updateTime` DATE COMMENT '更新时间',
+   PRIMARY KEY ( `id` )
+) COMMENT = '评论表' ENGINE = InnoDB DEFAULT CHARSET = utf8;
